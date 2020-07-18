1e0813a2edcad64e32254543c600abc686d9fcb0
HolkerDev <nick.penkovskiy@gmail.com>
2020-07-19 00:16:07 +0200

    Added conf and start service

--- src/main/kotlin/com/awe/music/services/EntryService.kt
+++ src/main/kotlin/com/awe/music/services/EntryService.kt
@@ -1,10 +1,16 @@
 package com.awe.music.services
 
+import org.apache.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
 
 @Service
 class EntryService @Autowired constructor() {
+    private val _log = Logger.getLogger(EntryService::class.java)
 
-
+    @KafkaListener
+    fun sendRegistrationMail(email: String) {
+        TODO("Write send email logic")
+    }
 }
